f937e23f480e1d67ad702903be7fab6fa2af785f
CanAhmet12 <can65385@gmail.com>
2024-06-06 18:00:58 +0300

    as
    as

---
+++ Kütüphane Uygulması Ödev/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kütüphane_Uygulması_Ödev
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            if(textBox1.Text == "admin" && textBox2.Text == "12345")
+            {
+                Form2 yeni = new Form2();
+                yeni.Show();
+                this.Hide();
+
+
+            }
+            else
+            {
+                MessageBox.Show("Yanliş Kullanıcı adı veya şifre girdiniz Lütfen tekrar deneyiniz!");
+
+            }
+
+
+
+
+        }
+    }
+}
